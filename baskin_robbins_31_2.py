56eeb2b7341bc8f3ba81ec36a20c2e6726358a26
RatelXD <jhou8221@gmail.com>
2025-11-17 19:21:23 +0900

    베스킨 라벤스 N 수정

--- baskin_robbins_31_2.py
+++ baskin_robbins_31_2.py
@@ -2,36 +2,25 @@ max, min, target = map(int, input().split())
 a, b = 0, 0
 
 while(True): 
-    while(True):
-        # for i in range (2, target, max+1):
-        #     if (i<=b):
-        #         continue # i를 출력하지 않고 넘어감 # i-=i로 하면 i의 자리가 0으로 출력
-        #     print(i, end=' ')
+    while(True): 
         
-        print("\n A 입력: ", end='')
-        a_tmp = int(input()) # a_tmp에 임시값을 넣고 조건 충족되면 a로 대입
-        if a_tmp>=b+min and a_tmp <= b+max and a_tmp<=target-1:
-            a = a_tmp
-            print(a)
+        print("A 입력: ", end='')
+        a = int(input()) # a_tmp에 임시값을 넣고 조건 충족되면 a로 대입
+        if a>=b+min and a <= b+max and a<=target-1:
             break
         else:
             print("잘못입력했습니다.")
-    if a == target-1:
-        print("A 승리")
-        break 
+    for j in range(2, target, max-1):
+        if a == j:
+            for i in range (2, target, max+1):
+                if (i<=b):
+                    continue # i를 출력하지 않고 넘어감 # i-=i로 하면 i의 자리가 0으로 출력
+                print(i, end=' ')
+            break 
+        else:
+            print("-1")
+            break
+
 
     
-    while(True):
-        print("B 입력: ", end='')
-        b_tmp = int(input())
-        if b_tmp>=a+min and b_tmp <= a+max and b_tmp<=target-1:
-            b=b_tmp
-            print(b)
-            break   
-        else:
-            print("잘못입력했습니다.")
-
-    if  b == target-1:
-        print("-1")
-        print("B 승리")
-        break
+    
