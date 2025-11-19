c2b0fd87c223452c9c23c7e83495f759202a7712
RatelXD <jhou8221@gmail.com>
2025-11-19 12:56:49 +0900

    베스킨 라벤스 N 수정

--- baskin_robbins_31_2.py
+++ baskin_robbins_31_2.py
@@ -1,26 +1,18 @@
-max, min, target = map(int, input().split())
-a, b = 0, 0
-
-while(True): 
-    while(True): 
-        
-        print("A 입력: ", end='')
-        a = int(input()) # a_tmp에 임시값을 넣고 조건 충족되면 a로 대입
-        if a>=b+min and a <= b+max and a<=target-1:
-            break
-        else:
-            print("잘못입력했습니다.")
-    for j in range(2, target, max-1):
-        if a == j:
-            for i in range (2, target, max+1):
-                if (i<=b):
-                    continue # i를 출력하지 않고 넘어감 # i-=i로 하면 i의 자리가 0으로 출력
-                print(i, end=' ')
+print("규칙 제시 A: ", end ='')
+min, max, target = map(int, input().split())
+print("B: ", end = '')
+if (min < max) and (max * 10 < target) and (min < 10000) and (max < 10001) and (target < 200000): 
+    for i in range(((target-min)%(min+max)), (target-min)+1, (min+max)):
+        if min <= i <= max:
+            a = list(range(((target-min)%(min+max)), (target-min)+1, (min+max)))
+            print(*a)
             break 
         else:
             print("-1")
             break
-
+        
+# max * 10 < target 게임이 최소 10턴은 진행되어야함
+# (target-min)%(min+max)
 
     
     
